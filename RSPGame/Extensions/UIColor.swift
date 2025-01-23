7997ddcca90873842967b6ccaa42e83b989f5224
Kvend-ac <kvenii@ya.ru>
2025-01-23 16:34:41 +0300

    Added background, added title label

---
+++ RSPGame/Extensions/UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor.swift
+//  RSPGame
+//
+//  Created by Сергей Прасолов on 23.01.2025.
+//
+
+import UIKit
+
+extension UIColor {
+    static let textColor = UIColor(red: 64 / 255,
+                                   green: 81 / 255,
+                                   blue: 81 / 255,
+                                   alpha: 1)
+}
--- RSPGame/ViewController.swift
+++ RSPGame/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let backgroundImageView = UIImageView()
+    private let titleLabelView = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        pipelineUI()
     }
-
 
 }
 
+// MARK: - Setup UI
+private extension ViewController {
+    
+    func pipelineUI() {
+        view.addSubview(backgroundImageView)
+        view.addSubview(titleLabelView)
+        
+        configureBackgroundImageView()
+        configureTitleLabelView()
+    }
+    
+    func configureBackgroundImageView() {
+        
+        backgroundImageView.image = .background1
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    func configureTitleLabelView() {
+        
+        titleLabelView.text = "Rock Paper Scissors"
+        titleLabelView.textColor = .textColor
+        titleLabelView.font = UIFont(name: "BerkshireSwash-Regular", size: 50)
+        titleLabelView.adjustsFontSizeToFitWidth = true
+        titleLabelView.textAlignment = .center
+        titleLabelView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            titleLabelView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            titleLabelView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabelView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            titleLabelView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.07)
+        ])
+        
+        
+    }
+    
+}
+
+#Preview {
+    ViewController()
+}
