7c8e165ebe0e19ba86d8c1792163f5f57f076435
Kvend-ac <kvenii@ya.ru>
2025-01-26 21:36:16 +0300

    Added project colors (yellow, orange, green)

--- RSPGame/Extensions/UIColor.swift
+++ RSPGame/Extensions/UIColor.swift
@@ -12,8 +12,24 @@ extension UIColor {
                                    green: 81 / 255,
                                    blue: 81 / 255,
                                    alpha: 1)
+    
     static let darkButtonBackgroundColor = UIColor(red: 26 / 255,
                                                    green: 56 / 255,
                                                    blue: 56 / 255,
                                                    alpha: 1)
+    
+    static let yellowProjectColor = UIColor(red: 234 / 255,
+                                            green: 186 / 255,
+                                            blue: 0 / 255,
+                                            alpha: 1)
+    
+    static let orangeProjectColor = UIColor(red: 240 / 255,
+                                            green: 120 / 255,
+                                            blue: 0 / 255,
+                                            alpha: 1)
+    
+    static let greenProjectColor = UIColor(red: 120 / 255,
+                                            green: 129 / 255,
+                                            blue: 0 / 255,
+                                            alpha: 1)
 }
